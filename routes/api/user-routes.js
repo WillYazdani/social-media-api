2e149adba9cf33661c591569fd2e99890dd4edfb
Will Yazdani <yazd0006@gmail.com>
2022-05-01 18:36:52 -0500

    added user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+//user routes
+const {
+    getAllUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../controllers/user-controller');
+
+// get all and post
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);;
+// get one, put and delete
+router
+    .route('/:id')
+    .get(getOneUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
