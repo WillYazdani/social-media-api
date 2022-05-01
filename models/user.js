9ea2d02cc42442649f30e714bace652483a49646
Will Yazdani <yazd0006@gmail.com>
2022-05-01 17:33:53 -0500

    added user model

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const {Schema, model, Types} = require('mongoose');
+const dateFormat = require('../utils/date-format');
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [this]
+    },
+    {
+    
+        toJSON : {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+        timestamps: true
+});
+
+UserSchema.virtual('friends').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
