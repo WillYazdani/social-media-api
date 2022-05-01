ad309aff9e46c2b24d3ba2a30185fad3833b24cc
Will Yazdani <yazd0006@gmail.com>
2022-05-01 13:42:16 -0500

    updated controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,76 +1,82 @@
 const {user, thought} = require('../models/user');
 
 const userController = {
-    // GET all users
-    getAllUsers: (req, res) => {
-        user.find({}, (err, users) => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.status(200).send(users);
-            }
+   //get all users
+   getAllUsers(req, res) {
+       user.find({})
+       .populate({
+              path: 'thoughts',
+              select: '-__v'
+         })
+         .select('-__v')
+         .sort({_id: -1})
+         .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+               console.log(err);
+               res.status(400).json(err);
+            });
+    },
+    //get one user
+    getOneUser(req, res) {
+        user.findOne({_id: req.params.id})
+        .populate({
+               path: 'thoughts',
+               select: '-__v'
+          })
+          .select('-__v')
+          .then(dbUserData => {
+             if(!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'});
+                return;
+             }
+             res.json(dbUserData);
+          })
+          .catch(err => {
+             console.log(err);
+             res.status(400).json(err);
+          });
+    },
+    //create a user
+    createUser(req, res) {
+        user.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
         });
     },
-    // GET a user by id
-    getUserById: (req, res) => {
-        user.findById(req.params.id, (err, user) => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.status(200).send(user);
+    //update a user
+    updateUser(req, res) {
+        user.findOneAndUpdate({_id: req.params.id}, {new: true, runValidators: true})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'});
+                return;
             }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
         });
     },
-    // create a user
-    createUser: (req, res) => {
-        user.create(req.body, (err, user) => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.status(200).send(user);
+    //delte a user
+    deleteUser(req, res) {
+        user.findOneAndDelete({_id: req.params.id})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'});
+                return;
             }
-        });
-    },
-    // update a user
-    updateUser: (req, res) => {
-        user.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.status(200).send(user);
-            }
-        });
-    },
-    // delete a user
-    deleteUser: (req, res) => {
-        user.findByIdAndRemove(req.params.id, (err, user) => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.status(200).send(user);
-            }
-        });
-    },
-    // add friend
-    addFriends: (req, res) => {
-        user.findByIdAndUpdate(req.params.id, {$push: {friends: req.body}}, (err, user) => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.status(200).send(user);
-            }
-        });
-    },
-    // delete friend
-    deleteFriend: (req, res) => {
-        user.findByIdAndUpdate(req.params.id, {$pull: {friends: req.body}}, (err, user) => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.status(200).send(user);
-            }
-        });
+            res.json(dbUserData);
+        })
     }
 }
+   
+
 
 module.exports = userController;
