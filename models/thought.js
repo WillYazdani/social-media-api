0fc7fe0db5623c4ad03ebfc96099b6c5f59fb644
Will Yazdani <yazd0006@gmail.com>
2022-05-01 20:50:42 -0500

    fixed tought model bug

--- models/thought.js
+++ models/thought.js
@@ -1,68 +1,75 @@
-const {Schema, model, Types} = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 //add reaction schema
-const reactionSchema = new Schema({
+const reactionSchema = new Schema(
+  {
     reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId(),
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
     reactionBody: {
-        type: String,
-        required: true,
-        maxLength: 280
+      type: String,
+      required: true,
+      maxLength: 280,
     },
     username: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
-        toJSON : {
-            getters: true
-        },
-        id: false
-});
+
+    id: false,
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 //add thought schema
-const ThoughtSchema = new Schema({
+const ThoughtSchema = new Schema(
+  {
     thoughtText: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: [1, 'Thought must be at least 1 character long'],
-        maxLength: [280, 'Thought must be less than 280 characters long']
+      type: String,
+      required: true,
+      trim: true,
+      minLength: [1, "Thought must be at least 1 character long"],
+      maxLength: [280, "Thought must be less than 280 characters long"],
     },
     createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    reactions: [reactionSchema]
-},
-{
-    toJSON : {
-        virtuals: true,
-        getters: true
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
     },
     id: false,
-});
+  }
+);
 
 //ThoughtSchema virtual
-ThoughtSchema.virtial('reactionCount').get(function() {
-    return this.reactions.length;
-});
+ ThoughtSchema.virtual("reactionCount").get(function () {
+   return this.reactions.length;
+ });
 
 //Thought model
-const Thought = model('Thought', ThoughtSchema);
+const Thought = model("Thought", ThoughtSchema);
 
 //export
 module.exports = Thought;
